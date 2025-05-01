cd461ac72d78bb079cc9b476d9a006300d35b812
ralf <ralf47450@gmail.com>
2025-05-01 18:47:09 +0300

    favicon add

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
-
+export const metadata = {
+  title: "Мой сайт",
+  description: "Описание сайта",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 export default function App({ Component, pageProps }) {
   return <div className="bg-[#0d0d0d] text-white"><Component {...pageProps} /></div>
 }
