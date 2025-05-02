2e8a846b6a8b2c60d666ba24368dbd804c297f77
ralf <ralf47450@gmail.com>
2025-05-02 10:06:30 +0300

    grainy texture carbon fix metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
-export const metadata = {
-  title: "Мой сайт",
-  description: "Описание сайта",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+// export const metadata = {
+//   title: "Мой сайт",
+//   description: "Описание сайта",
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+// };
 export default function App({ Component, pageProps }) {
   return <div className="bg-noise text-white"><Component {...pageProps} /></div>
 }
